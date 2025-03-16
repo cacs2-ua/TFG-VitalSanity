fc5104d67009626af176c08e3142338a0f81e101
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-03-16 15:41:24 +0100

    login funciona bien para usuarios pacientes

--- src/main/java/vitalsanity/security/SecurityConfig.java
+++ src/main/java/vitalsanity/security/SecurityConfig.java
@@ -1,8 +1,7 @@
-package vitalsanity.config;
+package vitalsanity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
