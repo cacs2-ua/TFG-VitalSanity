cc069193d5d322c2f24e7a089dc4733360cd825d
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-04-07 15:56:38 +0200

    bug corregido en ProfesionalMedicoRepository

--- src/main/java/vitalsanity/repository/ProfesionalMedicoRepository.java
+++ src/main/java/vitalsanity/repository/ProfesionalMedicoRepository.java
@@ -2,10 +2,11 @@ package vitalsanity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import vitalsanity.model.Paciente;
+import vitalsanity.model.ProfesionalMedico;
 import vitalsanity.model.Usuario;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ProfesionalMedicoRepository extends JpaRepository<Paciente, Long> {
+public interface ProfesionalMedicoRepository extends JpaRepository<ProfesionalMedico, Long> {
 }
